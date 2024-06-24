3858b5e4fe255779896f72ecbdff3c9ffd155960
Guido Espinoza <guidoespinozac@gmail.com>
2024-06-23 21:58:32 -0400

    setting database

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -85,12 +85,12 @@ const invoices = [
     status: 'pending',
     date: '2023-08-05',
   },
-  {
-    customer_id: customers[7].id,
-    amount: 54246,
-    status: 'pending',
-    date: '2023-07-16',
-  },
+  // {
+  //   customer_id: customers[7].id,
+  //   amount: 54246,
+  //   status: 'pending',
+  //   date: '2023-07-16',
+  // },
   {
     customer_id: customers[0].id,
     amount: 666,
