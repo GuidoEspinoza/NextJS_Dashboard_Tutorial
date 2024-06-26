c2ac59d33da10d8511f024319e5efc5e60d2831a
Guido Espinoza <guidoespinozac@gmail.com>
2024-06-25 21:34:11 -0400

    término capítulo 9

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 import {
   UserGroupIcon,
   HomeIcon,
@@ -21,7 +21,7 @@ const links = [
 ];
 
 export default function NavLinks() {
-  const pathName = usePathname()
+  const pathName = usePathname();
   return (
     <>
       {links.map((link) => {
@@ -34,7 +34,7 @@ export default function NavLinks() {
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
                 'bg-sky-100 text-blue-600': pathName === link.href,
-              }
+              },
             )}
           >
             <LinkIcon className="w-6" />
