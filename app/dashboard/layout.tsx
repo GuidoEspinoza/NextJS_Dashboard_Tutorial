cc0d68c401b9d5905bbecda3c624c3fba56bdc9f
Guido Espinoza <guidoespinozac@gmail.com>
2024-06-27 19:38:39 -0400

    término capítulo 11

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,6 @@
 import SideNav from '../ui/dashboard/sidenav';
 
-export const experimental_ppr = true
+export const experimental_ppr = true;
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
