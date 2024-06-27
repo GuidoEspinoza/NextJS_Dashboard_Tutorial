6c16618ef1e0d741ead0ee7cc1fffaaaa4a9d392
Guido Espinoza <guidoespinozac@gmail.com>
2024-06-27 18:56:22 -0400

    término capítulo 10

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 import SideNav from '../ui/dashboard/sidenav';
 
+export const experimental_ppr = true
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="cd:overflow-hidden flex h-screen flex-col md:flex-row">
