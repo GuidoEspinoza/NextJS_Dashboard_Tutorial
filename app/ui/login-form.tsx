0ff812f1958f46c9cf59a17052a2d02a8671789f
Guido Espinoza <guidoespinozac@gmail.com>
2024-06-28 18:45:19 -0400

    deploy final

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -8,11 +8,21 @@ import {
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from './button';
-import { useFormState } from 'react-dom';
+import { useFormState, useFormStatus } from 'react-dom';
 import { authenticate } from '@/app/lib/actions';
 
 export default function LoginForm() {
   const [errorMessage, formAction] = useFormState(authenticate, undefined);
+
+  function LoginButton() {
+    const { pending } = useFormStatus();
+
+    return (
+      <Button className="mt-4 w-full" aria-disabled={pending}>
+        Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+      </Button>
+    );
+  }
   return (
     <form action={formAction} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
@@ -60,9 +70,7 @@ export default function LoginForm() {
             </div>
           </div>
         </div>
-        <Button className="mt-4 w-full">
-          Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-        </Button>
+        <LoginButton />
         <div
           className="flex h-8 items-end space-x-1"
           aria-live="polite"
